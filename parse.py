c842fcdd85912a419fa1eb712a60ea120ea33cc1
iandioch <iandioch11@gmail.com>
2017-01-14 20:34:23 -0500

    Add parse.py

---
+++ parse.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import sys
+
+import pyocr
+import pyocr.builders
+
+
+tools = pyocr.get_available_tools()
+if len(tools) == 0:
+    print("Error: No OCR tool found")
+    sys.exit(1)
+
+# should be 'Tesseract (sh)'
+tool = tools[0]
+
+orig_image = Image.open('test.png')
+
+# crop to only the section with the number of problems solved
+cropped_image = orig_image.crop((47, 40, 97, 60))
+
+# double the size of the image so the OCR has more to go on
+resized_image = cropped_image.resize((100, 40), Image.ANTIALIAS)
+
+digits = tool.image_to_string(
+        resized_image,
+        builder=pyocr.tesseract.DigitBuilder()
+)
+
+print(digits)
