569593b7ad4440531f090a032219abe08bf67be8
iandioch <iandioch11@gmail.com>
2017-01-15 14:41:51 -0500

    Add extra test for michcioperz badge

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import parse
 
 class TestParse(unittest.TestCase):
     def test_parse(self):
-        self.assertEqual(parse.parse('test.png'), '128')
+        self.assertEqual(parse.parse('tests/iandioch.png'), '128')
+        self.assertEqual(parse.parse('tests/michcioperz.png'), '25')
 
 if __name__ == '__main__':
     unittest.main()
