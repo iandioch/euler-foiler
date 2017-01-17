756fcdc5268af0cfc092ec42bcc0f95024226758
iandioch <iandioch11@gmail.com>
2017-01-17 14:08:28 -0500

    Fix tests to work with new parse func

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import unittest
 import parse
+from PIL import Image
 
 class TestParse(unittest.TestCase):
     def test_parse(self):
-        self.assertEqual(parse.parse('tests/iandioch.png'), '128')
-        self.assertEqual(parse.parse('tests/michcioperz.png'), '25')
+        self.assertEqual(parse.parse(parse.open_image('tests/iandioch.png')), '128')
+        self.assertEqual(parse.parse(parse.open_image('tests/michcioperz.png')), '25')
 
 if __name__ == '__main__':
     unittest.main()
